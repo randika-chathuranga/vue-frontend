c23c01e377447f4a8b93d4ed2d5ba3f2168dd2cf
Randika Chathuranga <randikachathuranga219@gmail.com>
2021-05-10 18:28:11 +0530

    Update Favourites.vue
    updated Go to the topics content

--- src/views/Favourites.vue
+++ src/views/Favourites.vue
@@ -17,7 +17,19 @@
 
                     <v-stepper-items>
                     <v-stepper-content step="1">
-                    <v-card  class="mb-12" color="grey lighten-1" height="200px"></v-card>
+                    <v-card  class="mb-12" color="grey lighten-1" height="200px">
+                        <v-row wrap flat class="pt-5 pb-5">
+                            <v-flex class="text-h5 mt-1 ml-7 font-weight-medium">Go to the topic</v-flex>
+                        </v-row>
+                        <v-divider></v-divider>
+                        <v-row wrap flat>
+                            <v-flex class="pa-2 mt-3 ml-3">
+                            Online advertising addresses ads placed on other websites, in email newsletters, or via affiliate programs to generate leads, increase website traffic, raise visibility and drive sales.
+                            Online advertising has numerous benefits, allowing you to reach a very broad or very targeted audience on multiple devices, from PCs to tablets to smart phones. It gives you flexibility in ad formats and sizes, 
+                            allowing for text ads, graphical ads, interactive ads and video. Different ad networks allow for different payment options, either on a cost per impression or cost per click format.
+                            </v-flex>
+                        </v-row>
+                    </v-card>
                     <v-btn color="primary" @click="e1 = 2">Continue</v-btn>
                     <v-btn text>Cancel</v-btn>
                     </v-stepper-content>
